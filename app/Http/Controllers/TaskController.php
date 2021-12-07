28bff68b759e755636aaeecb6f855441c7ee0b9c
Hsin-Ju Teng <dlulu900821@gmail.com>
2021-12-07 23:23:27 +0800

    修改TaskController & web.php 內容

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,8 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests;
-use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,6 @@ Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
 })->name('dashboard');
 
 //Route::get('/tasks', 'TaskController@index');
-Route::get('/tasks', \App\Http\Controllers\TaskController::class, 'index')->name('tasks.index');
+Route::get('/tasks', [\App\Http\Controllers\TaskController::class,'index'])->name('tasks.index');
 Route::post('/task', 'TaskController@store');
 Route::delete('/task/{task}', 'TaskController@destroy');
